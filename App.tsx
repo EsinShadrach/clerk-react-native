89ac9b4ede339457514caa918915d95690ad5256
EsinShadrach <esinshadrachrafe@gmail.com>
2024-01-22 12:09:38 +0100

    Completed

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  useUser,
+} from "@clerk/clerk-react";
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import SignInWithOAuth from "./components/SignInWithOauth";
 
 export default function App() {
+  const env = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY ?? "";
+
+  function getUser() {
+    const { user } = useUser();
+    if (user) {
+      return user.username;
+    }
+    return "no user";
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider publishableKey={env}>
+      <SafeAreaView style={styles.AndroidSafeArea}>
+        <View style={styles.centeredView}>
+          <SignedIn>
+            <Text>{getUser()}</Text>
+          </SignedIn>
+          <SignedOut>
+            <SignInWithOAuth />
+          </SignedOut>
+        </View>
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  AndroidSafeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
